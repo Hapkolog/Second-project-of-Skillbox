20e70f824d48b270d1f343efab3d4fe19f7e860b
XE <danik.burlai@gmail.com>
2024-04-23 20:53:35 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import telebot
+import config
+
+
+
+bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(commands=["start"])
+def hello_func(message):
+    bot.send_message(message.chat.id, 'Привет, бла-бла-бла')
+
+ 
+@bot.message_handler(commands=['low'])
+def low_info(message):
+    low_api_info = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search'
+
+    try:
+        response = requests.get(low_api_info)
+        bot.send_message(message.chat.id, f'Миниальное кол-во запросов: {response.headers.lower_items()}')
+
+    except Exception as e:
+        bot.send_message(message.chat.id, f'Произошла ошибка: {str(e)}')
+
+
+bot.polling(none_stop=True)
